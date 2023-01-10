dd1407619c0cca6affdd197ce0bbd0eb0d81bbc2
Ben Dubroff <BLDubroff@gmail.com>
2023-01-09 21:34:35 -0500

    function bugs

--- logic.js
+++ logic.js
@@ -4,8 +4,26 @@ warrentyHeader.style.fontWeight ='bold'
 
 document.body.appendChild(warrentyHeader)
 
-const firstNameLabel = document.createElement('label')
-firstNameLabel.textContent = 'First Name: '
-const firstNameInput = document.createElement('input')
-document.body.appendChild(firstNameLabel)
-document.body.appendChild(firstNameInput)
+// const firstNameLabel = document.createElement('label')
+// firstNameLabel.textContent = 'First Name: '
+// const firstNameInput = document.createElement('input')
+// document.body.appendChild(firstNameLabel)
+// document.body.appendChild(firstNameInput)
+
+function textForm(value) {
+    let label = document.createElement('label')
+    label.textContent = value
+    let input = document.createElement('input')
+    const lb = document.createElement('br')
+
+
+    document.body.appendChild(label)
+    document.body.appendChild(lb)
+    document.body.appendChild(input)
+    document.body.appendChild(lb)
+    document.body.appendChild(lb)
+}
+
+textForm('First Name:')
+textForm('Last Name:')
+textForm('Company Name:')
